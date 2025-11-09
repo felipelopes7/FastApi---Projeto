c190999e35a9f93537807ded9027e13b7a32cad1
unknown <felipelopes7000@gmail.com>
2025-11-09 17:44:33 -0400

    Projeto

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from  passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+from fastapi.security import OAuth2PasswordBearer
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCES_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCES_TOKEN_EXPIRE_MINUTES"))
+
+app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login-form")
+
+from auth_routes import auth_router
+from order_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
